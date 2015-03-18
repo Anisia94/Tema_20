0d8caf4ed2e2d7371d9ddf217b4dbfd6efa003b9
Anisia-Ioana <anisia.lupu@yahoo.com>
2015-03-18 12:07:29 +0200

    Initial commit

---
+++ src/LoveME.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Anisia-Ioana on 3/18/2015.
+ */
+public class LoveME {
+    public LoveME() {
+    }
+    static  void main(String[] args){
+        System.out.println("Love");
+    }
+}
